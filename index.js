4e1a3e8777f6997a0859f0a82f99734b3bb67ef6
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-08-18 14:08:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 function fetchBooks() {
-  return fetch('https://anapioficeandfire.com/api/books')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(json) {
-    renderBooks(json)
-    
-
-    
-  });
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    // the below .then we are calling the function renderBooks that we made 
+    // it takes an array in this case books is the json we are getting back 
+    //  than we are invoking it in the first function fetchBooks 
+    // the fetchBooks function is being invoked by the event listeneter DOMcontent
+    // load where its gonna be invoked when everything is getting loaded
+    .then(json => renderBooks(json))
 }
+
 function renderBooks(books) {
   const main = document.querySelector('main')
   books.forEach(book => {
